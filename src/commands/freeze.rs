6d9684dc5a90d575be49ff81e6e0d17b129bb262
accup <32096121+accup@users.noreply.github.com>
2023-08-27 23:17:35 +0900

    optout bwt

--- src/commands/freeze.rs
+++ src/commands/freeze.rs
@@ -1,10 +1,6 @@
 use clap::Args;
 
-use azuki::core::{
-    bwt::bwt,
-    lz77::LZ77,
-    suffix_array::{suffix_array, U8Bucket},
-};
+use azuki::core::lz77::LZ77;
 
 use crate::commands::{
     io::{with_extension, Reading, Writing},
@@ -20,9 +16,6 @@ pub struct FreezeCommandArgs {
 
     #[arg(short, long)]
     pub output: Option<String>,
-
-    #[arg(long)]
-    pub bwt: bool,
 }
 
 impl Command for FreezeCommand {
@@ -31,26 +24,12 @@ impl Command for FreezeCommand {
     fn execute(&self, args: &Self::Args) -> std::io::Result<()> {
         let input_path = args.input.clone();
         let output_path = args.output.clone();
-        let output_path = output_path.or({
-            let mut path = input_path.clone();
-            if args.bwt {
-                path = with_extension(path.as_deref(), "bwt");
-            }
-            path = with_extension(path.as_deref(), "frozen");
-            path
-        });
+        let output_path = output_path.or(with_extension(input_path.as_deref(), "frozen"));
 
         let mut reading = Reading::open(input_path.as_deref())?;
         let mut writing = Writing::create(output_path.as_deref())?;
 
-        let mut data = reading.read_data()?;
-
-        if args.bwt {
-            let suffix_array = suffix_array(&data, &U8Bucket);
-            data = bwt(&data, &suffix_array);
-        }
-
-        LZ77::compress(&data, &mut writing)?;
+        LZ77::compress(&reading.read_data()?, &mut writing)?;
 
         Ok(())
     }
